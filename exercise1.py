b96109463dae38f7109be31f7be775ad7f439dc7
Chandramauli - <s2041305@baltic10.geos.ed.ac.uk>
2021-01-13 10:24:08 +0000

    printing calender

---
+++ exercise1.py
@@ -0,0 +1,5 @@
+print("Let's Print a Calender")
+for week in range(1,53):
+    print("Week: ",week)
+    for day in ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]:
+        print("Day:",day)
